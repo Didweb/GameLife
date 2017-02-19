9cc508ca676d8a3a93d4d6f393612362fa2672e8
Edu <info@did-web.com>
2017-02-19 23:38:25 +0100

    Create class cell

---
+++ src/cell/cell.java
@@ -0,0 +1,29 @@
+package cell;
+
+public class cell {
+
+	
+	private boolean state;
+	
+	
+	public cell(){
+		
+		state = false;
+	}
+	
+	
+	public boolean getStateCell(){
+	
+		return state;
+	}
+	
+	
+	
+	public void setStateCell(boolean state){
+		
+		this.state = state;
+	}
+	
+	
+	
+}
